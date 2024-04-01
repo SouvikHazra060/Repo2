31d90631b2fdac4cf8e06c4e3d6dd1924fb1bbd3
Souvik Hazra <132296054+SouvikHazra060@users.noreply.github.com>
2024-04-01 16:12:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Students...')
+  res.send('Hello Souvik...')
 })
 
 app.listen(4000, ()=>{
